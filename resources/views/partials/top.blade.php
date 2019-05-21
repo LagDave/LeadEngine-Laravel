5386c4caea72226c9f1f05c8dadab6d0f82e0895
rdavephp <rdavephp@gmail.com>
2019-05-21 12:24:55 -0700

    changed font

--- resources/views/partials/top.blade.php
+++ resources/views/partials/top.blade.php
@@ -12,14 +12,15 @@
         {{-- Fontawesome --}}
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
               integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-        {{-- Barlow|Montserrat|Poppins|Wire One --}}
-        <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:700|Poppins"
-              rel="stylesheet">
+
         {{-- Roboto --}}
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
         {{-- Acme --}}
-        <link href="https://fonts.googleapis.com/css?family=Acme&display=swap" rel="stylesheet">
+        {{-- <link href="https://fonts.googleapis.com/css?family=Acme&display=swap" rel="stylesheet"> --}}
+
+        {{-- Open Sans Condensed --}}
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300&display=swap" rel="stylesheet">
 
     {{-- Remote Script --}}
         {{-- JQuery --}}
