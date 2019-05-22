3e8fa376eaa5d498f6d012ba66dcbce5f0134f3d
rdavephp <rdavephp@gmail.com>
2019-05-21 19:39:46 -0700

    changed case study images

--- resources/views/components/jumbotron.blade.php
+++ resources/views/components/jumbotron.blade.php
@@ -12,13 +12,13 @@
             <div class='nav-items'>
 
 
-                <a class='text-primary  text-small nav-item' href="">Home</a>
+                <a class='text-primary text-small nav-item' href="">Home</a>
                 <a target='_blank' href='https://calendly.com/leadengine-support/15min'
-                   class='text-primary  text-small nav-item'>Schedule Call</a>
-                <a onclick='showPricingPage()' class='text-primary  text-small nav-item'>Pricing</a>
-                <a onclick='showCaseStudyPage()' class='text-primary  text-small nav-item'>Case Study</a>
-                <a href='{{route('signup.index')}}' class='text-primary  text-small nav-item'>Sign Up</a>
-                <a href='https://calendly.com/leadengine-support/15min' class='text-primary text-small  nav-item' >Free Consultation</a>
+                   class='text-primary text-small nav-item'>Schedule Call</a>
+                <a onclick='showPricingPage()' class='text-primary text-small nav-item'>Pricing</a>
+                <a onclick='showCaseStudyPage()' class='text-primary text-small nav-item'>Case Study</a>
+                <a href='{{route('signup.index')}}' class='text-primary text-small nav-item'>Sign Up</a>
+                <a href='https://calendly.com/leadengine-support/15min' class='text-primary text-small   nav-item' >Free Consultation</a>
 
             </div>
             <button class="hamburger btn btn-transparent"><i class="fas fa-bars"></i></button>
