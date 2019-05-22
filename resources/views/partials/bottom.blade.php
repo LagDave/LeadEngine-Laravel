81ae2e16526bdf78de2e0da6cf164645e78257df
rdavephp <rdavephp@gmail.com>
2019-05-21 19:18:57 -0700

    added google analytics script tag

--- resources/views/partials/bottom.blade.php
+++ resources/views/partials/bottom.blade.php
@@ -4,3 +4,15 @@
 {{-- Scripts --}}
 <script src="{{asset('js/scripts.js')}}"></script>
 <script src='{{asset('js/components_summoner.js')}}'></script>
+
+
+{{-- Google Analytics Script --}}
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-140474016-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+ 
+  gtag('config', 'UA-140474016-1');
+</script>
