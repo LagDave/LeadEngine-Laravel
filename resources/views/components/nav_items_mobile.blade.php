ac54fb5c4b1a77090656b5b30dce4e30c0745e07
rdavephp <rdavephp@gmail.com>
2019-05-24 08:11:58 -0700

    Updated Mobile Navigation container

--- resources/views/components/nav_items_mobile.blade.php
+++ resources/views/components/nav_items_mobile.blade.php
@@ -1,9 +1,11 @@
 <div class="nav-items-mobile">
     <a class='text-primary text-normal nav-item' href="{{route('home')}}">Home</a>
-    <a target='_blank' href='https://calendly.com/leadengine-support/15min'
-       class='text-primary text-normal nav-item'>Schedule Call</a>
+    <a href='http://web.leadengine.live/faq' class='text-primary text-small   nav-item' >FAQ</a>
+
     <a onclick='showPricingPage()' class='text-primary text-normal nav-item'>Pricing</a>
     <a onclick='showCaseStudyPage()' class='text-primary text-normal nav-item'>Case Study</a>
     <a href='{{route('signup.index')}}' class='text-primary text-normal nav-item'>Sign Up</a>
+    <a target='_blank' href='https://calendly.com/leadengine-support/15min'
+    class='text-primary text-normal nav-item'>Free Consultation</a>
 
 </div>
