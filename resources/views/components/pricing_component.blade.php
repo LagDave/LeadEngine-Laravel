d14a3eae2e070d0772729ae9e15b146636435e52
rdavephp <rdavephp@gmail.com>
2019-06-15 15:47:37 -0700

    Updated Pricing

--- resources/views/components/pricing_component.blade.php
+++ resources/views/components/pricing_component.blade.php
@@ -21,56 +21,118 @@
         </p>
         <br>
         <div class="pricing">
-                <p class="text-grey text-bold text-medium text-center text-primary">PRICING</p>
-            <div class="price-and-calculator-container">
-                
-                <div class="price-text-container">
-                    <div class="pricing-text">
-                        <p class="text-mednorm text-center">
-                            <span class="text-grey text-bold text-medium text-primary price">
-                                <sup style='font-size:.6em'>$ </sup>
-                                <span class="price-value"> 165</span><span class="cent">.00</span>
-                            </span>
-                        </p>
-                        <p class="text-mednorm text-primary text-bold text-purple text-center comment"></p>
-                    </div>
-                    <div class="pricing-perks">
-                        <p class="text-small text-primary"><i class="text-mednorm fas fa-check-square text-green"></i> 30-Day Money-Back Guarantee</p>
-                        <p class="text-small text-primary"><i class="text-mednorm fas fa-check-square text-green"></i> Cancel anytime</p>
-                        <p class="text-small text-primary"><i class="text-mednorm fas fa-check-square text-green"></i> No Set up fees</p>
+            <p class="text-grey text-bold text-medium text-center text-primary">PRICING</p>
+            
+            <div class="pricing-wrapper">
+                <div class="price-and-calculator-container">
+                    <p style='background:linear-gradient(45deg, purple 50%, darkblue );; margin:0; border-radius:5px 5px 0 0; padding:30px 0;' class="text-white pricing-title text-mednorm text-primary text-center">STANDARD</p>
 
-                    
+                    <div class="pricing-stats-table">
+                        <table style='margin-top:30px;'>
+                            <tr>
+                                <td class='text-center text-primary text-grey'>100/day connection request or a total of 3,000/month</td>
+                            </tr>
+                            
+                            <tr>
+                                <td class='text-center text-primary text-grey'>500 to 700 contact details of new connections compiled in a CSV file  </td>
+                            </tr>
+                            
+                            <tr>
+                                <td class='text-center text-primary text-grey'>Custom auto-messaging to new Connections (OPTIONAL)</td>
+                            </tr>
+                        
+                            <tr>
+                                <td class='text-center text-primary text-grey'>Dedicated account manager</td>
+                            </tr>
+                        
+                            <tr>
+                                <td class='text-center text-primary text-grey'>24/7 technical support</td>
+                            </tr>
+                        
+                            <tr>
+                                <td class='text-center text-primary text-grey'>Inbox cleaner</td>
+                            </tr>
+                        </table>
+                        
+                    </div>
+                    <div class="price-text-container">
+
+                        <div class="pricing-text">
+                            <p class="text-mednorm text-center">
+                                <span class="text-grey text-bold text-medium text-primary price">
+                                    <sup style='font-size:.6em'>$ </sup>
+                                    <span class="price-value"> 165</span><span class="cent">.00</span>
+                                </span>
+                            </p>
+                            <p class="text-mednorm text-primary text-bold text-purple text-center comment"></p>
+                        </div>
+
+                    </div>
+
+                    <div class="pricing-select">
+                        <select class='price-select-input text-primary'>
+                            <option value='monthly' span class='text-primary'>Monthly</span></option>
+                            <option value='semiannually' span class='text-primary'>6 Months</span></option>
+                            <option value='annually' span class='text-primary'>12 Months</span></option>
+                        </select>
                     </div>
                 </div>
-                <br>
 
-                <div class="pricing-select">
-                    <select class='price-select-input text-primary'>
-                        <option value='monthly' span class='text-primary'>Monthly</span></option>
-                        <option value='semiannually' span class='text-primary'>6 Months</span></option>
-                        <option value='annually' span class='text-primary'>12 Months</span></option>
-                    </select>
+                <div class="price-and-calculator-container">
+                    <p style='background:linear-gradient(45deg, purple 50%, darkblue );; margin:0; border-radius:5px 5px 0 0; padding:30px 0;' class="text-white pricing-title text-mednorm text-primary text-center">PREMIUM</p>
+
+                    <div class="pricing-stats-table">
+                        <table  style='margin-top:30px;width:100%;'>
+                            <tr>
+                                <td class='text-center text-primary text-grey'>All Features of Standard Plan</td>
+                            </tr>
+                            
+                            <tr>
+                                <td class='text-center text-medium text-bold text-primary'>+</td>
+                            </tr>
+                            
+                            <tr>
+                                <td class='text-center text-primary text-grey'>Email Marketing Campaign</td>
+                            </tr>
+                        
+                            <tr>
+                                <td class='text-center text-primary text-grey'>Follow up automation</td>
+                            </tr>
+                        
+                            <tr>
+                                <td class='text-center text-primary text-grey'>Custom Email Template</td>
+                            </tr>
+                        </table>
+                        
+                    </div>
+
+                    <div class="price-text-container">
+
+                        <div class="pricing-text">
+                            <p class="text-mednorm text-center">
+                                <span class="text-grey text-bold text-medium text-primary price">
+                                    <sup style='font-size:.6em'>$ </sup>
+                                    <span class=""> 214</span><span class="">.00</span>
+                                </span>
+                            </p>
+                            <p class="text-mednorm text-primary text-bold text-purple text-center"></p>
+                        </div>
+
+                    </div>
+
+                    <div class="pricing-select">
+                        <select style="background:#999;" disabled class='price-select-input text-primary'>
+                            <option value='monthly' span class='text-primary'>Monthly</span></option>
+                            <option value='semiannually' span class='text-primary'>6 Months</span></option>
+                            <option value='annually' span class='text-primary'>12 Months</span></option>
+                        </select>
+                    </div>
                 </div>
             </div>
             
-            <br>
-            <br>
-            
-            <p class='text-dbl-line-height text-normal' style='max-width:600px; margin:0 auto'><span class="text-green"><i class="fas fa-check-circle"></i></span> <span class="text-grey text-primary"> 100/day connection request or  3,000/month connection request per campaign</span></p>
-            <br>
-            <p class='text-dbl-line-height text-normal' style='max-width:600px; margin:0 auto'><span class="text-green"><i class="fas fa-check-circle"></i></span> <span class="text-grey text-primary">500 to 700 contact details of new connections compiled in a CSV file</span></p>
-            <br>
-            <p class='text-dbl-line-height text-normal' style='max-width:600px; margin:0 auto'><span class="text-green"><i class="fas fa-check-circle"></i></span> <span class="text-grey text-primary">Custom auto-messaging to new connections (Optional)</span></p>
-            <br>
-            <p class='text-dbl-line-height text-normal' style='max-width:600px; margin:0 auto'><span class="text-green"><i class="fas fa-check-circle"></i></span> <span class="text-grey text-primary">Dedicated account manager</span></p>
-            <br>
-            <p class='text-dbl-line-height text-normal' style='max-width:600px; margin:0 auto'><span class="text-green"><i class="fas fa-check-circle"></i></span> <span class="text-grey text-primary">24/7 technical support</span></p>
-            <br>
-            <p class='text-dbl-line-height text-normal' style='max-width:600px; margin:0 auto'><span class="text-green"><i class="fas fa-check-circle"></i></span> <span class="text-grey text-primary">Inbox cleaner</span></p>
-
-            <br>
+            <br><br>
             <p class="text-center">
-                <button class="btn btn-blue">Free Consultation</button>
+                <button style='width:60%;' class="btn btn-blue">Free Consultation</button>
             </p>
 
 
