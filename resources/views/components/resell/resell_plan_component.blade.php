1e942d28133bd3a4fe9a088ca3120182d847f80d
rdavephp <rdavephp@gmail.com>
2019-06-16 08:53:34 -0700

    updated the pricing appearance

--- resources/views/components/resell/resell_plan_component.blade.php
+++ resources/views/components/resell/resell_plan_component.blade.php
@@ -55,7 +55,7 @@
                         </div>
     
                         <br>
-                        <div class='white-label-checkout-container' style='width:100%; display:flex; align-items:center; justify-content:center; transform: scale(1.3, 1.3'>
+                        <div class='white-label-checkout-container' style='width:100%; display:flex; align-items:center; justify-content:center; transform: scale(1.3, 1.3)'>
                         </div>
                         
                     </div>
@@ -110,12 +110,15 @@
                         </div>
     
                         <br>
-                        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                            <input type="hidden" name="cmd" value="_s-xclick">
-                            <input type="hidden" name="hosted_button_id" value="PEKHES4L35RZS">
-                            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-                        </form>
+                        <div style='width:100%; display:flex; align-items:center; justify-content:center; transform: scale(1.3, 1.3)'>
+                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                                <input type="hidden" name="cmd" value="_s-xclick">
+                                <input type="hidden" name="hosted_button_id" value="PEKHES4L35RZS">
+                                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                            </form>
+                        </div>
+                        
                         
                     </div>
                 </div>
